7d94cc5371c40519d6dbc9eecf72504c9b543b10
Paulo Monteiro <paulofelipemonteiro@gmail.com>
2017-01-27 00:01:53 -0200

    gulpfile documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,73 @@
 var gulp = require("gulp");
 var notify = require("gulp-notify");
 
-var pug = require("gulp-pug");
+var flatten = require("gulp-flatten");
+
 var sass = require("gulp-sass");
 var uglify = require("gulp-uglify");
 
 var paths = {
-	resources:{
-		folder: "./resources/"
-	},
+    resources: {
+        folder: "./resources/"
+    },
 
-	public:{
-		folder: "./public/"
-	},
+    public: {
+        folder: "./extension/"
+    },
 
-	node:{
-		folder: "./node_modules/"
-	}
+    node: {
+        folder: "./node_modules/"
+    }
 };
 
-paths.resources.view	= paths.resources.folder + "view/*.pug";
-paths.resources.sass 	= paths.resources.folder + "sass/*.scss";
-paths.resources.js 		= paths.resources.folder + "js/**/*.js";
-paths.resources.icons	= paths.resources.folder + "logos_tech/";	
+paths.resources.sass = paths.resources.folder + "sass/*.scss";
+paths.resources.js = paths.resources.folder + "js/**/*.js";
+paths.resources.icons = paths.resources.folder + "logos_tech/";
 
-paths.node.angular		= paths.node.folder + "angular/**/*.js";
-paths.node.ngAnimate	= paths.node.folder + "angular-animate/**/*.js";
+paths.node.angular = paths.node.folder + "angular*/**/*.min.js";
 
-paths.public.view		= paths.public.folder;
-paths.public.css 		= paths.public.folder + "css";
-paths.public.js 		= paths.public.folder + "js/";
+paths.public.view = paths.public.folder;
+paths.public.css = paths.public.folder + "css";
+paths.public.js = paths.public.folder + "js";
 
 var infos = {
-	name: "TR Chrome Extension"
+    name: "TR SUPPORT TOOLS"
 }
 
-gulp.task("default", ["view", "sass", "js", "watch"], function(){
-	
+gulp.task("default", ["sass", "js", "watch"], function() {
+
 });
 
-gulp.task('watch', function(){
-	gulp.watch(paths.resources.sass, ["sass"]);
-	gulp.watch(paths.resources.js, ["js"]);
+gulp.task('watch', function() {
+    gulp.watch(paths.resources.sass, ["sass"]);
+    gulp.watch(paths.resources.js, ["js"]);
 })
 
-gulp.task("sass", function(){
-	gulp.src(paths.resources.sass)
-		.pipe(sass({outputStyle: "extended"}))
-		.pipe(gulp.dest(paths.public.css))
-		.pipe(notify({
-		        "title": "Project web site: " + infos.name,
-		        "subtitle": "SASS - COMPILED",
-		        "message": "<%= file.relative %>",
-		        "sound": "Frog", // case sensitive
-		        //"icon": paths.resources.icons + "sass_logo.png", // case sensitive
-		        "onLast": true,
-      		})
-		);
+gulp.task("sass", function() {
+    gulp.src(paths.resources.sass)
+        .pipe(sass({ outputStyle: "extended" }))
+        .pipe(gulp.dest(paths.public.css))
+        .pipe(notify({
+            "title": "Project web site: " + infos.name,
+            "subtitle": "SASS - COMPILED",
+            "message": "<%= file.relative %>",
+            "sound": "Frog", // case sensitive
+            //"icon": paths.resources.icons + "sass_logo.png", // case sensitive
+            "onLast": true,
+        }));
 })
 
-gulp.task("js", function(){
-	gulp.src([paths.resources.js, paths.node.angular, paths.node.ngAnimate])
-		//.pipe(uglify())
-		.pipe(gulp.dest(paths.public.js))
-		.pipe(notify({
-		        "title": "Project web site: " + infos.name,
-		        "subtitle": "JS - COMPILED",
-		        "message": "<%= file.relative %>",
-		        "sound": "Frog", // case sensitive
-		        //"icon": paths.resources.icons + "sass_logo.png", // case sensitive
-		        "onLast": true,
-      		})
-		);
+gulp.task("js", function() {
+    gulp.src([paths.resources.js, paths.node.angular])
+        //.pipe(uglify())
+        .pipe(flatten())
+        .pipe(gulp.dest(paths.public.js))
+        .pipe(notify({
+            "title": "Project web site: " + infos.name,
+            "subtitle": "JS - COMPILED",
+            "message": "<%= file.relative %>",
+            "sound": "Frog", // case sensitive
+            //"icon": paths.resources.icons + "sass_logo.png", // case sensitive
+            "onLast": true,
+        }));
 })
