2fba2f42f5e9b368fba761b3ec17f81a4c6bc90b
Paulo Monteiro <paulofelipemonteiro@gmail.com>
2017-02-06 00:02:31 -0200

    add pug engine

--- gulpfile.js
+++ gulpfile.js
@@ -43,86 +43,126 @@
  *                             *
  * ****************************/
 var gulp = require("gulp");
-var plugins = require("gulp-load-plugins")();
+var $ = require("gulp-load-plugins")();
 
 var options = {
-    info: {
-        name: "TR SUPPORT TOOLS"
-    },
-
-    minified: false,
-
-    paths: {
-        nodeFolder: "./node_modules/",
-
-        resourcesFolder: "./resources/",
-        resources: {
-            angular: "./node_modules/angular*/**/*.min.js",
-            bootstrap: "./node_modules/bootstrap/dist/**/*.min",
-            componentsControllers: ".resources/components/**/*.js",
-            componentsTemplates: ".resources/components/**/*.html",
-            sass: "./resources/sass/*.scss",
-            js: "./resources/js/**/*.js",
-            icons: "./resources/logos_tech/"
-        },
-        
-        publicFolder: "./extension/",
-        public: {
-            css: "./extension/css/",
-            js: "./extension/js/",
-            components: "./extension/components/"
-        }
-    }
+    name: "TR SUPPORT TOOLS"
 };
 
+var paths = {
+    nodeFolder: "./node_modules/",
+    resourcesFolder: "./resources/",
+    publicFolder: "./extension/"
+};
+
+var angular = {
+    angular: "./node_modules/angular*/**/*.min.js",
+    componentsControllers: "./resources/components/**/*.js",
+    componentsTemplates: "./resources/components/**/*.pug"
+};
+
+var resources = {
+    bootstrap: "./node_modules/bootstrap/dist/**/*.min",
+    view: "./resources/views/*.pug",
+    sass: "./resources/sass/*.scss",
+    js: "./resources/js/**/*.js",
+    icons: "./resources/logos_tech/"
+};
+
+var dist = {
+    build: false,
+
+    view: "./extension/",
+    css: "./extension/css/",
+    js: "./extension/js/",
+    components: "./extension/components/"
+};
+
+
+/*******************************
+ *                             *
+ * 			Tasks              *
+ *                             *
+ * ****************************/
 gulp.task("default", ["sass", "js", "components", "watch"], function() {
 
 });
 
 gulp.task("watch", function() {
-    gulp.watch(options.paths.resources.sass, ["sass"]);
-    gulp.watch(options.paths.resources.js, ["js"]);
+    gulp.watch(resources.views, ["view"]);
+    gulp.watch(resources.sass, ["sass"]);
+    gulp.watch(resources.js, ["js"]);
+})
+
+gulp.task("view", function() {
+    gulp.src(resources.view)
+        .pipe($.if(dist.build, $.pug(), $.pug({ pretty: true })))
+        .pipe($.flatten())
+        .pipe(gulp.dest(dist.view))
+        .pipe($.notify({
+            "title": "Project web site: " + options.name,
+            "subtitle": "VIEW - COMPILED",
+            "message": "<%= file.relative %>",
+            "sound": "Frog",
+            "icon": resources.icons + "pug_logo.png",
+            "onLast": true,
+        }));
 })
 
 gulp.task("sass", function() {
-    gulp.src([options.paths.resources.sass, options.paths.resources.bootstrap + ".css"])
-        .pipe(plugins.sass({ outputStyle: "extended" }))
-        .pipe(plugins.flatten())
-        .pipe(gulp.dest(options.paths.public.css))
-        .pipe(plugins.notify({
-            "title": "Project web site: " + options.info.name,
+    gulp.src([resources.sass, resources.bootstrap + ".css"])
+        .pipe($.if(dist.build, $.sass(), $.sass({ outputStyle: "extended" })))
+        .pipe($.flatten())
+        .pipe(gulp.dest(dist.css))
+        .pipe($.notify({
+            "title": "Project web site: " + options.name,
             "subtitle": "SASS - COMPILED",
             "message": "<%= file.relative %>",
             "sound": "Frog",
-            "icon": options.paths.resources.icons + "sass_logo.png",
+            "icon": resources.icons + "sass_logo.png",
             "onLast": true,
         }));
 })
 
 gulp.task("js", function() {
-    gulp.src([options.paths.resources.js, options.paths.resources.angular, options.paths.resources.bootstrap + ".js"])
-        .pipe(plugins.if(options.minified, plugins.uglify()))
-        .pipe(plugins.flatten())
-        .pipe(gulp.dest(options.paths.public.js))
-        .pipe(plugins.notify({
-            "title": "Project web site: " + options.info.name,
+    gulp.src([resources.js, , resources.bootstrap + ".js"])
+        .pipe($.if(dist.build, $.uglify()))
+        .pipe($.flatten())
+        .pipe(gulp.dest(dist.js))
+        .pipe($.notify({
+            "title": "Project web site: " + options.name,
             "subtitle": "JS - COMPILED",
             "message": "<%= file.relative %>",
             "sound": "Frog",
-            "icon": options.paths.resources.icons + "sass_logo.png",
+            "icon": resources.icons + "sass_logo.png",
             "onLast": true,
         }));
 });
 
-gulp.task("components", function(){
-    gulp.src(options.paths.resources.componentsTemplates)
-        .pipe(gulp.dest(options.paths.public.components))
-        .pipe(plugins.notify({
-            "title": "Project web site: " + options.info.name,
-            "subtitle": "COMPONENTES - COPIED",
+gulp.task("components:controllers", function() {
+    gulp.src(angular.componentsControllers)
+        .pipe($.if(dist.build, $.uglify()))
+        .pipe(gulp.dest(dist.components))
+        .pipe($.notify({
+            "title": "Project web site: " + options.name,
+            "subtitle": "COMPONENTES CONTROLLER - COMPILED",
             "message": "<%= file.relative %>",
             "sound": "Frog",
-            "icon": options.paths.resources.icons + "js_logo.png",
+            "icon": resources.icons + "js_logo.png",
+            "onLast": true,
+        }));
+});
+
+gulp.task("components:templates", function() {
+    gulp.src(angular.componentsTemplates)
+        .pipe($.if(dist.build, $.pug(), $.pug({ pretty: true })))
+        .pipe(gulp.dest(dist.components))
+        .pipe($.notify({
+            "title": "Project web site: " + options.name,
+            "subtitle": "COMPONENTES TEMPLATES - COMPILED",
+            "message": "<%= file.relative %>",
+            "sound": "Frog",
+            "icon": resources.icons + "pug_logo.png",
             "onLast": true,
         }));
 });
